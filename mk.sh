9a93f02752567ae91715524048ea393a694e0c8a
Gabriel Pereira de Carvalho <nazter57@usp.br>
2022-08-02 19:24:39 -0300

    :boom: em andamento

--- mk.sh
+++ mk.sh
@@ -1,6 +1,5 @@
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -g kbd.c -o kbd.o
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -g t.c -o t.o
+arm-none-eabi-gcc -c -mcpu=arm926ej-s -g super.c -o super.o
 arm-none-eabi-as -c -mcpu=arm926ej-s -g ts.s -o ts.o
-arm-none-eabi-ld -T t.ld kbd.o t.o ts.o -o t.elf
+arm-none-eabi-ld -T t.ld ts.o super.o -o t.elf
 arm-none-eabi-objcopy -O binary t.elf t.bin
 qemu-system-arm -M versatilepb -m 128M -kernel t.bin -nographic -serial /dev/null
